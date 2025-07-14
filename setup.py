5c79656dedc2ec72f00b735245dc3bde66182afd
Javid Shiriyev, Ph.D. <jshiriyev@utexas.edu>
2025-07-14 20:26:02 +0400

    .

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = 'wellx',
+	version = '0.0.1',
+	packages = find_packages(),
+	install_requires = [
+		'numpy>=1.26.4',
+		],
+	)
+
+# Run the followings from the command line to test it locally:
+
+# python setup.py sdist bdist_wheel
+
+# pip install dist/wellx-{version}-py3-none-any.whl
+
+# Run the followings from the command line to upload to pypi:
+
+# twine upload dist/*
