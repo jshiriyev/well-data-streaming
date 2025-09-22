44c8e2f973fa64e48d8c1a1f42d6a1610100965e
Javid Shiriyev, Ph.D. <jshiriyev@utexas.edu>
2025-09-22 17:09:07 +0400

    .

--- build/lib/wellx/maps/sidebars/__init__.py
+++
@@ -1,6 +0,0 @@
-from ._filter_sidebar import FilterSidebar
-
-from ._attach_attrs import AttachAttrs
-from ._multi_filter import MultiFilter
-
-from ._categorical_filter_sidebar import CategoricalFilterSidebar
